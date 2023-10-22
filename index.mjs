6f41ba9ee6ed359a7886d74d884e7e02f06f97c9
Tyler <tylergross28@gmail.com>
2023-10-22 13:09:52 -0400

    don't use nested stuff

---
+++ index.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env zx
+
+const [_nodePath, _zxPath, _scriptPath, fileOrFolderName] = process.argv;
+console.log(fileOrFolderName);
--- src/index.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-const [, , fileOrFolderName] = process.argv;
-console.log(fileOrFolderName);
